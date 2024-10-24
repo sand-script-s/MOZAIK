498d0103eebe96b82b58f768f14fae2a6abf32e2
sand-script-s <sand.script.s@protonmail.com>
2024-10-24 16:22:42 +0200

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# MOZAIK - Collage Filmstrips
+
+## About
+
+These works present some my own explorations, using free-association and collage technique, into realms of coincidence/synchronicity, and their potential as tools towards novel invention, insight and therapy.
+
+Most of it was done by haphazardously stringing together a handful of videos based on some or other communal theme, such as a shared object, pattern, or personal association, and then looking for myself to see what new coincidences (such as puns) arise sponaneously from that arrangement.
+I was often astounded by unexpected and thought-provoking coincidences and as such I am sharing these in the hopes that others may be inspired thereby, and try their own hand at various such methods. They may perhaps be astounded by what emerges quite unexpectedly from their own collages.
---
+++ assets/scripts/js/menubar.js
@@ -0,0 +1,98 @@
+const hamburgerdiv = document.getElementById('hamburgerdiv');
+const sliderdiv = document.getElementById('sliderdiv');
+
+const toplinespan = document.getElementById('toplinespan');
+const middlelinespan = document.getElementById('middlelinespan');
+const bottomlinespan = document.getElementById('bottomlinespan');
+
+let displayswitch = 0;
+
+let initialLoadHandled = false;
+
+hamburgerdiv.addEventListener('click', event => {
+    
+    if (displayswitch === 1) {
+        sliderdiv.classList.remove('slider-visible');
+        sliderdiv.classList.add('slider-hidden');
+        displayswitch = 0;
+
+        toplinespan.classList.remove('top_line-state-2');
+        toplinespan.classList.add('top_line-state-1');
+        middlelinespan.classList.remove('middle_line-state-2');
+        middlelinespan.classList.add('middle_line-state-1');
+        bottomlinespan.classList.remove('bottom_line-state-2');
+        bottomlinespan.classList.add('bottom_line-state-1');
+
+    } else {
+        sliderdiv.classList.remove('slider-hidden');
+        sliderdiv.classList.add('slider-visible');
+        displayswitch = 1;
+
+        toplinespan.classList.remove('top_line-state-1');
+        toplinespan.classList.add('top_line-state-2');
+        middlelinespan.classList.remove('middle_line-state-1');
+        middlelinespan.classList.add('middle_line-state-2');
+        bottomlinespan.classList.remove('bottom_line-state-1');
+        bottomlinespan.classList.add('bottom_line-state-2');
+    }
+
+});
+
+//------------------------------------------------------------------------------------------
+
+// Function to check screen width and adjust menu accordingly
+function adjustMenuBasedOnScreenWidth() {
+    const screenWidth = window.innerWidth; // Get current screen width
+    const thresholdWidth = 880; // Define the threshold width
+
+    if (screenWidth > thresholdWidth && initialLoadHandled == false) {
+        // Open the menu and adjust the hamburger icon if screen width is more than the threshold
+        sliderdiv.classList.add('no-animation');
+
+        sliderdiv.classList.remove('slider-hidden');
+        sliderdiv.classList.add('slider-visible');
+
+        toplinespan.classList.remove('top_line-state-1');
+        toplinespan.classList.add('top_line-state-2');
+        middlelinespan.classList.remove('middle_line-state-1');
+        middlelinespan.classList.add('middle_line-state-2');
+        bottomlinespan.classList.remove('bottom_line-state-1');
+        bottomlinespan.classList.add('bottom_line-state-2');
+
+        displayswitch = 1; // Update the switch to reflect the open state
+        initialLoadHandled = true;
+
+        globalThis.setTimeout(() => {
+            sliderdiv.classList.remove('no-animation');
+        }, 100);
+
+    }
+}
+
+// Call the function on page load
+document.addEventListener('DOMContentLoaded', adjustMenuBasedOnScreenWidth);
+
+//------------------------------------------------------------------------------------------
+
+$(document).ready(function(){
+
+    // jquery function to toggle sub menus on click
+    $('.sub-button').click(function() {
+        $(this).next('.sub-menu').slideToggle();
+        $(this).find('.svgprop').toggleClass('rotate');
+    })
+
+    // Function to open specific sub-menus on page load based on page ID
+    function openSubMenu(pageId, subMenuIndex) {
+        if ($('body').attr('id') === pageId) {
+            // subMenuIndex is optional, if you have multiple sub-menus and want to target a specific one
+            // If not provided, it defaults to the first sub-menu
+            var subMenuSelector = (subMenuIndex!== undefined)? `.sub-button:eq(${subMenuIndex})` : `.sub-button:first`;
+            $(subMenuSelector).next('.sub-menu').slideDown(); // Use slideDown to show, not slideToggle
+            $(subMenuSelector).find('.svgprop').addClass('rotate'); // Add the rotate class
+        }
+    } 
+
+    openSubMenu('invitationpage', 0); // Opens the first sub-menu on the homepage
+
+})
